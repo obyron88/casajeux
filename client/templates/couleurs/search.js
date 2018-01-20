85cee6e9fff43cc15c8de03cbf22c291c42664e1
obyron88 <coda2017@yahoo.com>
2018-01-20 17:37:18 +0100

    multiple search?

--- client/templates/couleurs/search.js
+++ client/templates/couleurs/search.js
@@ -3,25 +3,51 @@ Template.search.events({
         e.preventDefault();
         Session.set('manaFilter', parseInt($('.mana').val()));
         Session.set('priceFilter', parseInt($('.price').val()));
-        // Session.set('raretéFilter', $('.rareté').val());
+        Session.set('raretéFilter', $('.rareté').val());
     }
 });
 
 Template.search.helpers({
     results: function() {
+
         var s = '';
         var i = parseInt(s);
-        if (Session.equals('manaFilter', i)) {
+        var u = undefined;
+        var ui = parseInt(u);
+        var mana = 'manaFilter';
+        var price = 'priceFilter';
+        var rareté = 'raretéFilter';
+        var test = Session.equals(mana, i);
+        var test2 = Session.equals(price, i);
+
+        if (Session.equals(mana, i) || Session.equals(mana, ui)) {
             return Blanc.find({
-                price: Session.get('priceFilter'),
-                // mana: Session.get('manaFilter')
+                price: Session.get(price),
+                rareté: Session.get(rareté),
+            });
+        }
+        if (Session.equals(price, i) || Session.equals(price, ui)) {
+            return Blanc.find({
+                mana: Session.get(mana),
+                rareté: Session.get(rareté),
+            });
+        }
+        if (Session.equals(rareté, '')) {
+            return Blanc.find({
+                mana: Session.get(mana),
+                price: Session.get(price),
+            });
+        }
+        if (test || test2) {
+            return Blanc.find({
+                rareté: Session.get(rareté),
             });
         }
         else {
             return Blanc.find({
-                mana: Session.get('manaFilter'),
-                price: Session.get('priceFilter'),
-                // rareté: Session.get('raretéFilter')
+                mana: Session.get(mana),
+                price: Session.get(price),
+                rareté: Session.get(rareté),
             });
         }
     }
