bdc56bbd3753dc1af4eceedf8ad1c7a1f272da72
obyron88 <coda2017@yahoo.com>
2018-01-23 16:18:44 +0100

    multiple search progress

--- client/templates/couleurs/search.js
+++ client/templates/couleurs/search.js
@@ -4,10 +4,50 @@ Template.search.events({
         Session.set('manaFilter', parseInt($('.mana').val()));
         Session.set('priceFilter', parseInt($('.price').val()));
         Session.set('raretéFilter', $('.rareté').val());
+        Session.set('typeFilter', $('.type').val());
+        Session.set('éditionFilter', $('.édition').val());
+
     }
 });
 
 Template.search.helpers({
+     // _.uniq évite les doublons
+    type: function (){ return _.uniq(Blanc.find({}, {
+    // sort tri par ordre alphabétique avec 1 (-1 tri ordre inversé)/ fields cherche dans la collection les fields type
+    sort: {type: 1}, fields: {type: true}
+    }).map(function(x) {
+    return x.type;
+    }), true);
+    },
+
+    édition: function (){ return _.uniq(Blanc.find({}, {
+        sort: {édition: 1}, fields: {édition: true}
+    }).map(function(x) {
+        return x.édition;
+    }), true);
+    },
+
+    rareté: function (){ return _.uniq(Blanc.find({}, {
+        sort: {rareté: 1}, fields: {rareté: true}
+    }).map(function(x) {
+        return x.rareté;
+    }), true);
+    },
+
+    prix: function (){ return _.uniq(Blanc.find({}, {
+        sort: {price: 1}, fields: {price: true}
+    }).map(function(x) {
+        return x.price;
+    }), true);
+    },
+
+    mana: function (){ return _.uniq(Blanc.find({}, {
+        sort: {mana: 1}, fields: {mana: true}
+    }).map(function(x) {
+        return x.mana;
+    }), true);
+    },
+
     results: function() {
 
         var empty = '';
@@ -17,6 +57,8 @@ Template.search.helpers({
         var mana = 'manaFilter';
         var price = 'priceFilter';
         var rareté = 'raretéFilter';
+        var type = 'typeFilter';
+        var édition = 'éditionFilter';
         var manai = Session.equals(mana, i);
         var manaui = Session.equals(mana, ui);
         var pricei = Session.equals(price, i);
@@ -61,6 +103,8 @@ Template.search.helpers({
                 mana: Session.get(mana),
                 price: Session.get(price),
                 rareté: Session.get(rareté),
+                type: Session.get(type),
+                édition: Session.get(édition),
             });
         }
     }
