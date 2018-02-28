e6ee1c9e52feab75bddc53d60eb15f3911ed4211
obyron88 <coda2017@yahoo.com>
2018-02-28 11:27:25 +0100

    api

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 // rend le user avec l'id correspondant admin
 Meteor.startup(() => {
-    if (Meteor.users.findOne("5Aixz4QpHGhfvrv6W"))
-        Roles.addUsersToRoles("5Aixz4QpHGhfvrv6W", ['admin']);
+    if (Meteor.users.findOne("y98vF3qgtCfKMCrcN"))
+        Roles.addUsersToRoles("y98vF3qgtCfKMCrcN", ['admin']);
     // code to run on server at startup
 });
